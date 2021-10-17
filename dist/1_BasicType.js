51e0a46396975b75d90d3f1121161d253ef6f7f0
khanhtoandng <khanhtoan.dng@gmail.com>
2021-10-17 15:36:30 +0700

    basic types

---
+++ dist/1_BasicType.js
@@ -0,0 +1,9 @@
+"use strict";
+//  Basic Types
+let id = 5;
+let company = "Khanh Toan";
+let isNoob = true;
+let x = "dont use";
+let nothing = undefined;
+// Built-in Object
+let now = new Date();
---
+++ src/1_BasicType.ts
@@ -0,0 +1,9 @@
+//  Basic Types
+let id: number = 5;
+let company: string = "Khanh Toan";
+let isNoob: boolean = true;
+let x: any = "dont use";
+let nothing: undefined = undefined;
+
+// Built-in Object
+let now: Date = new Date();
